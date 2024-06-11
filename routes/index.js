c3a830231f18115280813da9c5e24bafd35e6c6e
Timothy Miamba <timal0361@gmail.com>
2024-06-11 10:27:23 +0200

    refactor: Update routes and views for displaying messages
    This commit updates the routes and views in the application to display messages on the home page. The `index.js` file in the `routes` directory is modified to pass an array of messages to the `index.ejs` view. The `index.ejs` view is also updated to iterate over the messages array and display each message's text, user, and added date.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
